64a74c2785c0417f402a7021ce400bbd85983679
rzhbadhon <rhbadhon030@gmail.com>
2025-11-08 00:31:52 +0600

    done

--- main.go
+++ main.go
@@ -35,5 +35,49 @@ func worker(wg *sync.WaitGroup, id int, jobs <-chan string, results chan<- strin
 
 
 func main(){
-		
+	urlsToChecks := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.github.com",
+		"https://golang.org",
+		"https://doesntexistdomainever.xyz", // random 
+		"https://www.youtube.com",
 	}
+
+	// creating channel and waitgroup
+	jobs := make(chan string, len(urlsToChecks))
+	results := make(chan string, len(urlsToChecks))
+	var wg sync.WaitGroup
+
+	// set workers
+	numWorkers := 3
+
+	// set wg for workers
+	wg.Add(numWorkers)
+	for w:=1; w <= numWorkers; w++{
+		go worker(&wg, w, jobs, results)
+	}
+
+	// send all job to jobs channel
+	for _, url := range urlsToChecks{
+		jobs <- url
+	}
+
+	// sending done now close jobs as no new task will be there...
+	close(jobs)
+
+	// another goroutine to close the results
+	go func ()  {
+		wg.Wait() // waits till workers finish the job
+		close(results)// closes after all works done	
+	}()
+
+	// lets print all results
+	fmt.Println("Results: ")
+	for res := range results{
+		fmt.Println(res)
+	}
+
+	fmt.Println("All done...")
+
+}
